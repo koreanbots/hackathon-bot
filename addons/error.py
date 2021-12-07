179851f4ea630f8424af2679751d61cc3f8c04fe
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2021-12-07 17:47:35 +0900

    feat(addons/error): Implemented error handler

---
+++ addons/error.py
@@ -0,0 +1,24 @@
+import traceback
+
+import dico_command
+
+
+class Error(dico_command.Addon):
+    @dico_command.on("command_error")
+    async def on_command_error(self, ctx: dico_command.Context, ex: Exception):
+        if isinstance(ex, dico_command.InvalidArgument):
+            await ctx.reply("❌ 인자값이 잘못되었습니다.")
+        elif isinstance(ex, dico_command.CheckFailed):
+            await ctx.reply("❌ 권한이 없습니다.")
+        else:
+            tb = ''.join(traceback.format_exception(type(ex), ex, ex.__traceback__))
+            edited_tb = ("..." + tb[-1985:]) if len(tb) > 2000 else tb
+            await ctx.reply(f"```py\n{edited_tb}\n```")
+
+
+def load(bot: dico_command.Bot):
+    bot.load_addons(Error)
+
+
+def unload(bot: dico_command.Bot):
+    bot.unload_addons(Error)
