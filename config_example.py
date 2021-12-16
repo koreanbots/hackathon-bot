5cb0e9967a86ca7be8eeb80a61e8e377dc4b1002
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2021-12-16 15:02:18 +0900

    feat(config): Add more config value

--- config_example.py
+++ config_example.py
@@ -1,2 +1,11 @@
+from dico import Snowflake
+
+
 class Config:
+    # Bot
     TOKEN: str = ""
+    GUILD_ID: Snowflake = Snowflake(696562545489870858)
+
+    # Vote
+    IDEATHON_NAME = "아이디어톤"
+    MAKETHON_NAME = "메이크톤"
