6c4f0eeb20686627b5b4ebaa1e6f27dfea16ed52
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2021-12-26 00:01:15 +0900

    feat(addons/utils): add ideathon/makethon close

--- addons/utils.py
+++ addons/utils.py
@@ -1,4 +1,7 @@
+import dico
 import dico_command
+
+from config import Config
 
 
 class Utils(dico_command.Addon):
@@ -9,6 +12,40 @@ class Utils(dico_command.Addon):
             oauth2 += "%20applications.commands"
         oauth2 += "&guild_id=911676954317582368"
         await ctx.reply(oauth2)
+
+    @dico_command.on("message_create")
+    async def on_message(self, message: dico.Message):
+        if message.channel_id != Config.IMAGE_CHANNEL_ID or not message.attachments:
+            return
+        await message.create_reaction("⭐")
+
+    @dico_command.command("close")
+    async def close(self, ctx: dico_command.Context):
+        await ctx.reply("아이디어톤과 메이크톤 중 하나를 선택해주세요.")
+
+    @close.subcommand("아이디어톤")
+    async def close_idea(self, ctx: dico_command.Context):
+        for channel in await self.bot.request_guild_channels(ctx.guild_id):
+            if channel.name == Config.IDEATHON_NAME:
+                parent = self.bot.get_channel(channel.parent_id)
+                if parent.name in Config.EXCLUDE_CATEGORIES:
+                    continue
+                ow = [n for n in channel.permission_overwrites if n.id != ctx.guild_id and n.id != Config.TEAM_ROLE][0]
+                ow.edit(send_messages=False)
+                await channel.edit_permissions(ow, reason="아이디어톤 종료")
+        await ctx.reply("✅ 아이디어톤을 종료했습니다.")
+
+    @close.subcommand("메이크톤")
+    async def close_idea(self, ctx: dico_command.Context):
+        for channel in await self.bot.request_guild_channels(ctx.guild_id):
+            if channel.name == Config.MAKETHON_NAME:
+                parent = self.bot.get_channel(channel.parent_id)
+                if parent.name in Config.EXCLUDE_CATEGORIES:
+                    continue
+                ow = [n for n in channel.permission_overwrites if n.id != ctx.guild_id and n.id != Config.TEAM_ROLE][0]
+                ow.edit(send_messages=False)
+                await channel.edit_permissions(ow, reason="메이크톤 종료")
+        await ctx.reply("✅ 메이크톤을 종료했습니다.")
 
 
 def load(bot: dico_command.Bot):
--- config_example.py
+++ config_example.py
@@ -1,12 +1,12 @@
 from typing import List
 
-from dico import Snowflake
+from dico import Snowflake, Role
 
 
 class Config:
     # Bot
     TOKEN: str = ""
-    GUILD_ID: Snowflake = Snowflake(911676954317582368)
+    GUILD_ID: Snowflake.TYPING = 911676954317582368
 
     # Vote
     IDEATHON_NAME: str = "아이디어톤"
@@ -14,3 +14,7 @@ class Config:
     MAX_IDEATHON_VOTES: int = 2
     MAX_MAKETHON_VOTES: int = 2
     EXCLUDE_CATEGORIES: List[str] = ["메뉴얼"]
+
+    # Utils
+    IMAGE_CHANNEL_ID: Snowflake.TYPING = 924280161413787668
+    TEAM_ROLE: Role.TYPING = 914163148351766578
